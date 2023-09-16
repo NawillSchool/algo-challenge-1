3bca6bbaa05b6da0399b3197eb119c2ef2005596
ushashir <laleoict@gmail.com>
2023-09-16 19:02:09 +0100

    first commit

---
+++ src/brackets/index.js
@@ -0,0 +1,27 @@
+/**
+ * Implement the solution to brackets
+ *
+ * @param {string} str The string of brackets.
+ * @returns {"valid" | "invalid"} Whether or not the string is valid.
+ */
+function isValid(param) {
+  let check = [];
+  let bracket = { ')': '(', '}': '{', ']': '['}; //Keys the closing brackets to the opening brackets for each bracket type
+  for (let i of param) {
+    if (!bracket[i]){ //Checks the individual indexs of th parameter against the values of the bracket object
+       check.push(i);
+    } else if (check.pop() !== bracket[i]) { // Checks if the opening bracket matches the closing bracket
+        return "invalid";
+    }
+  }
+  if (check.length === 0) { //If array length is 0 sequence of brackets is valid
+    return "valid";
+  } else {
+    return "invalid";
+  }
+}
+
+/*console.log(isValid(`{[()[]]()}()`))
+console.log(isValid(`[(])`))
+console.log(isValid("}{{{{}}}}{([])(((()))){"))*/
+module.exports = isValid;
---
+++ src/roman-numerals/index.js
@@ -0,0 +1,26 @@
+/**
+ * Implement the solution in this function
+ *
+ * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
+ * @returns {number} The decimal equivalent.
+ */
+function romanToDecimal(roman) {
+  let convert = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+
+  let arr = roman.toUpperCase().split(''); //Ignores user case and splits array
+  let decimal = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = convert[arr[i]];
+    let next = convert[arr[i+1]];
+
+    current < next ? decimal -= current : decimal += current; //Tenary operation
+  }
+  return decimal;
+}
+
+
+/*console.log(romanToDecimal(`XCVIII`))
+console.log(romanToDecimal(`XCIX`))
+console.log(romanToDecimal(`MCMXLIV`))*/
+module.exports = romanToDecimal;
